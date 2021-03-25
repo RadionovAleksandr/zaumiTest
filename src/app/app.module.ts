864302b2c41e9f7db63a7d6c59f9ed1b35d6561d
aradionov <aradionov@at-consulting.ru>
2021-03-25 18:38:59 +0700

    установил либу компонентов

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,15 @@ import { CreateTicketComponent } from './create-ticket/create-ticket.component';
 import { EditTicketComponent } from './edit-ticket/edit-ticket.component';
 import { TicketComponent } from './ticket/ticket.component';
 import { TicketRoutesComponent } from './ticket-routes/ticket-routes.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -18,9 +27,12 @@ import { TicketRoutesComponent } from './ticket-routes/ticket-routes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
