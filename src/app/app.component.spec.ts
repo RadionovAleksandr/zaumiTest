4d6648c40ec40b528add5ab153844fb1d40e1bb6
aradionov <aradionov@at-consulting.ru>
2021-03-30 09:14:57 +0700

    тест appComponent метод ng onInit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,15 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { TicketService } from './services/ticket.service';
+import { StoreService } from './services/store.service';
+import { ChangeDetectorRef } from '@angular/core';
+import { NzModalService } from 'ng-zorro-antd/modal';
+import { of } from 'rxjs';
+import { ITicket } from './inerfaces/ticket.interface';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -12,24 +19,36 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
+
+    const cd: ChangeDetectorRef = null;
+    const modalService = new NzModalService(null, null, null, null);
+    component = new AppComponent(new TicketService(null), new StoreService(), cd, modalService);
+
+    // fixture.detectChanges();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'test'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('test');
+  it('it get cites', () => {
+
+    const ticket = {
+      id: '1', placeOfDeparture: '',
+      dateOfDeparture: '',
+      placeOfArrival: '',
+      dateOfArrival: '',
+    };
+
+    const cities: string[] = ['', '', '', ''];
+    const tickets: ITicket[] = [ticket, ticket, ticket, ticket];
+    spyOn(component, 'getCities').and.callFake(() => of(cities).subscribe((respCities => component.citiesData = respCities)));
+    spyOn(component, 'getTickets').and.callFake(() => of(tickets).subscribe((respTickets => component.ticketslist = respTickets)));
+
+    component.ngOnInit();
+    expect(component.citiesData.length).toBe(4);
+    expect(component.ticketslist.length).toBe(4);
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('test app is running!');
-  });
+
 });
